c87042732a18159458255a32712d4fd44547d813
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-23 14:29:31 +0430

    business working calender utility library of Anar framework

---
+++ src/main/java/af/gov/anar/lib/businessworkingcalendar/AnarLibBusinessworkingcalendarApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.businessworkingcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibBusinessworkingcalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibBusinessworkingcalendarApplication.class, args);
+	}
+
+}
